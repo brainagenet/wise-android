3f0cabc797a6ba02101e5eba21b2009cda2c80ea
brainagenet <ms29.seo+dell64@gmail.com>
2011-07-19 14:24:12 +0900

    android.intent.action.MEDIA_MOUNTED event 발생시 package scan service를 호출하도록 기능 추가.

--- WiseInstaller/src/net/wisestone/installer/receiver/OnMediaMountedReceiver.java
+++ WiseInstaller/src/net/wisestone/installer/receiver/OnMediaMountedReceiver.java
@@ -1,23 +1,41 @@
-/**
- * 
+/*
+ * (#) net.wisestone.installer.receiver.OnMediaMountedReceiver.java
+ * Created on 2011. 7. 18.
  */
 package net.wisestone.installer.receiver;
+
+import net.wisestone.installer.service.ApplicationPackageScanService;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
- * @author exia2
- *
+ * 
+ * 
+ * @author ms29.seo@gmail.com
+ * @version 0.1
  */
-public class OnMediaMountedReceiver extends BroadcastReceiver {
+public class OnMediaMountedReceiver extends BroadcastReceiver
+{
 
-	/* (non-Javadoc)
-	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
-	 */
-	@Override
-	public void onReceive(Context arg0, Intent arg1) {
-	}
+    private static final String TAG = "OnMediaMountedReceiver";
+
+    private static final boolean LOGV = true;
+
+    /* (non-Javadoc)
+     * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+     */
+    public void onReceive(Context context, Intent intent) {
+        if (LOGV) {
+            Log.d(TAG,
+                    "onReceive(action=" + intent.getAction() + ", data=" + intent.getDataString()
+                            + ")");
+        }
+        
+        final Intent i = new Intent(context, ApplicationPackageScanService.class);
+        context.startService(i);
+    }
 
 }
